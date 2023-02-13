3ca0e7de012720f6c10a47959f5b885b23f8cc10
uvillanueva <ulysse.villanueva@epitech.eu>
2023-02-13 12:18:39 +0100

    [ADD] steps up to 3

--- server/mongo/User.ts
+++ server/mongo/User.ts
@@ -1,5 +1,27 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema();
+enum genders {
+    male,
+    female
+};
+
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    gender: {
+        type: genders,
+        required: false,
+    }
+});
 
 export const User = mongoose.model("User", UserSchema);
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import mongoose, { ConnectOptions }  from "mongoose";
 import { Auth } from "./routes/auth";
 import bodyparser from "body-parser"
 import cors from "cors";
+import * as Mongoose from "mongoose"
 
 const app = express();
 const port = 8888;
@@ -12,8 +13,24 @@ app.use(bodyparser.urlencoded({extended: true}))
 app.use(cors({origin: "http://localhost:8889", credentials: true}));
 app.use(Auth);
 
+const username = "username";
+const password = "password";
+const cluster = "cluster";
+const dbname = "db";
+
 app.get("/", async (req, res) => {
-    res.send("salut !");
+    mongoose.connect(
+        `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`,
+        {
+            useNewUrlParser: true
+        } as ConnectOptions
+    )
+    const db = mongoose.connection;
+    db.on("error", console.error.bind(console, "connection error: "));
+    db.once("open", function () {
+        console.log("Conneced successfully");
+    });
+    //res.send("salut !");
 });
 
 app.listen(port, function () {
